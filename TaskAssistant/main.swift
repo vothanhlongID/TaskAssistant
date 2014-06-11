50425b35d9571ffbf4da4c8993588057dee4b530
quachgia1106 <framgia@macbook-018-no-MacBook.local>
2014-06-11 17:41:12 +0700

    Initial Commit

---
+++ TaskAssistant/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  TaskAssistant
+//
+//  Created by framgia on 6/11/14.
+//  Copyright (c) 2014 __LongIndie___. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
